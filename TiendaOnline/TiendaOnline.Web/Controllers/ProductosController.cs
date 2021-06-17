d36c584035afbd426da8a461a334b769e0d9f9f7
Dany Galan <Dany Galan@DESKTOP-IP4M54Q>
2021-06-17 01:28:33 -0600

    Agrengando Arquitectura Basica

--- TiendaOnline/TiendaOnline.Web/Controllers/ProductosController.cs
+++ TiendaOnline/TiendaOnline.Web/Controllers/ProductosController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using TiendaOnline.Web.Models;
+using TiendaOnline.BL;
 
 namespace TiendaOnline.Web.Controllers
 {
@@ -12,24 +12,10 @@ namespace TiendaOnline.Web.Controllers
         // GET: Productos
         public ActionResult Index()
         {
-            var producto1 = new ProductoModel();
-            producto1.Id = 1;
-            producto1.Descripcion = "Ropa";
+            var productosBL = new ProductosBL();
+            var listadeproductos = productosBL.ObtenerProductos();
 
-            var producto2 = new ProductoModel();
-            producto2.Id = 2;
-            producto2.Descripcion = "Linea Blanca";
-
-            var producto3 = new ProductoModel();
-            producto3.Id = 3;
-            producto3.Descripcion = "Herramientas";
-
-            var listadeProductos = new List<ProductoModel>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
-
-            return View(listadeProductos);
+            return View(listadeproductos);
         }
     }
 }
