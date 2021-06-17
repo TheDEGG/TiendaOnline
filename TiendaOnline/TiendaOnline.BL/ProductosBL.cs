29f104fc67502ba52837e4088acd85208e3b7e0e
Dany Galan <Dany Galan@DESKTOP-IP4M54Q>
2021-06-17 01:58:18 -0600

    conectando BD

--- TiendaOnline/TiendaOnline.BL/ProductosBL.cs
+++ TiendaOnline/TiendaOnline.BL/ProductosBL.cs
@@ -8,31 +8,16 @@ namespace TiendaOnline.BL
 {
     public class ProductosBL
     {
+        Contexto _contexto;
+
+        public ProductosBL()
+        {
+            _contexto = new Contexto();
+        }
+
         public List<Producto> ObtenerProductos()
         {
-            var producto1 = new Producto();
-            producto1.Id = 1;
-            producto1.Descripcion = "Ropa";
-            producto1.Precio = 200;
-
-            var producto2 = new Producto();
-            producto2.Id = 2;
-            producto2.Descripcion = "Linea Blanca";
-            producto2.Precio = 250;
-
-
-            var producto3 = new Producto();
-            producto3.Id = 3;
-            producto3.Descripcion = "Herramientas";
-            producto3.Precio = 180;
-
-
-            var listadeProductos = new List<Producto>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
-
-            return listadeProductos;
+            return _contexto.Producto.ToList();
         }
     }
 }
