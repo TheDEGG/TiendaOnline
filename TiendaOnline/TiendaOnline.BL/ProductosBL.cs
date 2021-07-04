f51aacccad4c2ec0d11d2c7690e23d51d7eb8b63
Dany Galan <Dany Galan@DESKTOP-IP4M54Q>
2021-07-04 12:35:33 -0600

    Crear validaciones de sus mantenimientos y agregar imagenes a los mantenimientos donde aplique

--- TiendaOnline/TiendaOnline.BL/ProductosBL.cs
+++ TiendaOnline/TiendaOnline.BL/ProductosBL.cs
@@ -19,7 +19,7 @@ namespace TiendaOnline.BL
 
         public List<Producto> ObtenerProductos()
         {
-            ListadeProductos = _contexto.Productos.ToList();
+            ListadeProductos = _contexto.Productos.Include("Categoria").ToList();
             return ListadeProductos;
         }
 
@@ -32,16 +32,29 @@ namespace TiendaOnline.BL
             {
                 var productoExistente = _contexto.Productos.Find(producto.Id);
                 productoExistente.Descripcion = producto.Descripcion;
+                productoExistente.CategoriaId = producto.CategoriaId;
                 productoExistente.Precio = producto.Precio;
+                productoExistente.UrlImagen = producto.UrlImagen;
             }
             _contexto.SaveChanges();
         }
 
         public Producto ObtenerProducto(int id)
         {
-            var producto = _contexto.Productos.Find(id);
+            var producto = _contexto.Productos.Include("Categoria").FirstOrDefault(p => p.Id == id);
             return producto;
         }
+
+        //public List<Producto> ObtenerProductosActivos()
+        //{
+            //ListadeProductos = _contexto.Productos
+                //.Include(" Categoria ")
+                //.Where(r => r.Activo == true)
+                //.OrderBy(r => r.Descripcion)
+                //.ToList();
+
+            //return ListadeProductos;
+        //}
 
         public void EliminarProducto(int id)
         {
--- TiendaOnline/TiendaOnline.WebAdmin/Controllers/ProductosController.cs
+++ TiendaOnline/TiendaOnline.WebAdmin/Controllers/ProductosController.cs
@@ -10,13 +10,15 @@ namespace TiendaOnline.WebAdmin.Controllers
     public class ProductosController : Controller
     {
         ProductosBL _productosBL;
+        CategoriasBL _categoriasBL;
 
         public ProductosController()
         {
             _productosBL = new ProductosBL();
+            _categoriasBL = new CategoriasBL();
 
         }
-        // GET: Productos
+        // GET: Productos --- recuperan datos-DA LA PAGINA
         public ActionResult Index()
         {
             var ListadeProductos = _productosBL.ObtenerProductos();
@@ -28,40 +30,92 @@ namespace TiendaOnline.WebAdmin.Controllers
         public ActionResult Crear()
         {
             var nuevoProducto = new Producto();
+            var categorias = _categoriasBL.ObtenerCategorias();
+
+            ViewBag.CategoriaId = new SelectList(categorias, "Id", "Descripcion");
 
             return View(nuevoProducto);
         }
 
         [HttpPost]
-        public ActionResult Crear(Producto producto)
+        public ActionResult Crear(Producto producto, HttpPostedFileBase imagen)
         {
-            _productosBL.GuardarProducto(producto);
+            if (ModelState.IsValid)
+            {
+                if (producto.CategoriaId == 0)
+                {
+                    ModelState.AddModelError("CategoriaId", "Seleccione una categoria");
+                    return View(producto);
+                }
 
-            return RedirectToAction("Index");
+                if (imagen != null)
+                {
+                    producto.UrlImagen = GuardarImagen(imagen);
+                }
+
+                _productosBL.GuardarProducto(producto);
+
+                return RedirectToAction("Index");
+            }
+
+            var categorias = _categoriasBL.ObtenerCategorias();
+
+            ViewBag.CategoriaId = new SelectList(categorias, "Id", "Descripcion");
+            return View(producto);
+            
         }
 
         public ActionResult Editar(int id)
         {
             var producto =_productosBL.ObtenerProducto(id);
+            var categorias = _categoriasBL.ObtenerCategorias();
+
+            ViewBag.CategoriaId = new SelectList(categorias, "Id", "Descripcion", producto.CategoriaId);
+
             return View(producto);
         }
 
-        [HttpPost]
-        public ActionResult Editar(Producto producto)
+        [HttpPost]//envia datos al servidor
+        public ActionResult Editar(Producto producto, HttpPostedFileBase imagen)
         {
-            _productosBL.GuardarProducto(producto);
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                if (producto.CategoriaId == 0)
+                {
+                    ModelState.AddModelError("CategoriaId", "Seleccione una categoria");
+                    return View(producto);
+                }
+
+                if (imagen != null)
+                {
+                    producto.UrlImagen = GuardarImagen(imagen);
+                }
+
+
+                _productosBL.GuardarProducto(producto);
+
+                return RedirectToAction("Index");//una vez que se guarde se va a retornar
+            }
+
+
+            var categorias = _categoriasBL.ObtenerCategorias();
+
+            ViewBag.CategoriaId = new SelectList(categorias, "Id", "Descripcion");
+            return View(producto);            
         }
+
 
         public ActionResult Detalle(int id)
         {
             var producto = _productosBL.ObtenerProducto(id);
+            
             return View(producto);
         }
 
         public ActionResult Eliminar(int id)
         {
             var producto = _productosBL.ObtenerProducto(id);
+           
             return View(producto);
         }
 
@@ -70,6 +124,15 @@ namespace TiendaOnline.WebAdmin.Controllers
         {
             _productosBL.EliminarProducto(producto.Id);
             return RedirectToAction("Index");
+        }
+
+
+        private string GuardarImagen(HttpPostedFileBase imagen)
+        {
+            string path = Server.MapPath("~/Imagenes/" + imagen.FileName);
+            imagen.SaveAs(path);
+
+            return "/Imagenes/" + imagen.FileName;
         }
 
     }
